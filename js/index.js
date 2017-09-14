414dc9d5888a8674712a4485eeeb4cc3cd2f7298
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2017-09-14 11:15:30 +0200

    Added slide functionality

---
+++ js/index.js
@@ -0,0 +1,35 @@
+(function($){
+
+  var slider = $('.slides');
+
+  $(window).on('load', function(){
+    slider.bxSlider({
+      speed: 1000,
+      pager: false,
+      controls: false,
+      auto: false,
+      autostart: 'true',
+      mode: 'horizontal',
+      captions: false,
+      infiniteLoop: 'true',
+      sledeWidth: 100,
+      slideSelector: 'div.item'
+    });
+
+    //Use the arrow keys to control the slider
+    $(document).keydown(function(e){
+        if (e.keyCode == 39){
+            slider.goToNextSlide();
+            return false;
+        }
+        else if (e.keyCode == 37){
+            slider.goToPrevSlide();
+            return false;
+       }
+    });
+
+    //$('.main').stellar();
+
+  });
+
+})(jQuery);
